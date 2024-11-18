46657e8b4bc74f16e6997b4ba6eb6843ccdacaa7
Ace5tar <tace233@gmail.com>
2024-11-18 14:46:31 -0800

    Finalized VisionIOReal by adding support for 3 cameras

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -44,11 +44,10 @@ public final class Constants{
     REPLAY
   }
 
+  /** Constants used primarily for the vision subsystem */
   public static final class VisionConstants {
 
-    /**
-     * April tag field layout, inclues the positions of all static april tags as well as the size of the field.
-     */ 
+    /** April tag field layout, inclues the positions of all static april tags as well as the size of the field. */ 
     public static final AprilTagFieldLayout aprilTagFieldLayout;
     static {
       AprilTagFieldLayout layout = null;
@@ -70,6 +69,20 @@ public final class Constants{
       new Translation3d(0, 0, 0),
       new Rotation3d(0, 0, 0));
 
+    /** The name that connects the right camera in code to the right camera in photonvision on the rio */
+    public static final String rightCamName = "right";
+    /** Transform from the center of the robot to the right camera */
+    public static final Transform3d rightCamFromRobot = new Transform3d(
+      new Translation3d(0, 0, 0),
+      new Rotation3d(0, 0, 0));
+
+    /** The name that connects the left camera in code to the left camera in photonvision on the rio */
+    public static final String leftCamName = "left";
+    /** Transform from the center of the robot to the left camera */
+    public static final Transform3d leftCamFromRobot = new Transform3d(
+      new Translation3d(0, 0, 0),
+      new Rotation3d(0, 0, 0));
+
     // Following Values are only used for sim
     /* *Vertical resolution of the front camera */
     public static final int frontCamVertRes = 1000;
@@ -87,6 +100,40 @@ public final class Constants{
     public static final double frontCamAvgLatency = 0;
     /** Standard deviation in the latency fro the front camera */
     public static final double frontCamLatencyStdDev = 0;
+
+    /* *Vertical resolution of the right camera */
+    public static final int rightCamVertrtRes = 1000;
+    /** Horizontal resolution of the right camera */
+    public static final int rightCamHorizRes = 1000;
+    /** Right camera rotation offset. */
+    public static final Rotation2d rightCamRotation = Rotation2d.fromDegrees(90);
+    /** Right camera pixel error */
+    public static final double rightCamPxErr = 0;
+    /** Standard Deviation in the pixel error of the right camera */
+    public static final double rightCamPxErrStdDev = 0;
+    /** How many frames the right camera provides per second */
+    public static final double rightCamFPS = 60;
+    /** Average latency from the right camera */
+    public static final double rightCamAvgLatency = 0;
+    /** Standard deviation in the latency fro the right camera */
+    public static final double rightCamLatencyStdDev = 0;
+
+    /* *Vertical resolution of the left camera */
+    public static final int leftCamVertRes = 1000;
+    /** Horizontal resolution of the left camera */
+    public static final int leftCamHorizRes = 1000;
+    /** Left camera rotation offset. */
+    public static final Rotation2d leftCamRotation = Rotation2d.fromDegrees(90);
+    /** Left camera pixel error */
+    public static final double leftCamPxErr = 0;
+    /** Standard Deviation in the pixel error of the left camera */
+    public static final double leftCamPxErrStdDev = 0;
+    /** How many frames the left camera provides per second */
+    public static final double leftCamFPS = 60;
+    /** Average latency from the left camera */
+    public static final double leftCamAvgLatency = 0;
+    /** Standard deviation in the latency fro the left camera */
+    public static final double leftCamLatencyStdDev = 0;
 
   }
 }
