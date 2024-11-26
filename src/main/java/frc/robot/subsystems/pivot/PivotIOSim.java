b810de9c2b7bec0e0c4b41a981f2163bb30666ae
Ace5tar <tace233@gmail.com>
2024-11-26 15:07:17 -0800

    Minor pivot sim changes so that the code now actually builds

--- src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.pivot;
 
 import static frc.robot.Constants.PivotConstants.*;
 
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.ExponentialProfile;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
@@ -11,13 +10,12 @@ public class PivotIOSim implements PivotIO {
     private boolean openLoop = false;
     private double appliedVolts = 0.0;
     private final SingleJointedArmSim sim = pivotSim;
-
-    private final
-        
+    private ExponentialProfile.State pivotGoal;
+    private ExponentialProfile.State pivotSetpoint;
     
     public PivotIOSim() {
-        pivotGoal = new ExponentialProfile(pivotSimGoalPosition, 0);
-        pivotSetpoint = new ExponentialProfile(pivotSimSetpointPosition, 0);
+        pivotGoal = new ExponentialProfile.State(pivotSimGoalPosition, 0);
+        pivotSetpoint = new ExponentialProfile.State(pivotSimSetpointPosition, 0);
     }
 
     public void updateInputs(PivotIOInputs inputs) {}
